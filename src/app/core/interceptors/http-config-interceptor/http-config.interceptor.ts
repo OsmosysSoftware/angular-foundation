dec2d81ab5a71193460f3b3b600c6bb10c7b9538
ashutosh.b <ashutosh.b@osmosys.co>
2023-11-03 11:01:02 +0530

    fix: disable rule temporarily

--- src/app/core/interceptors/http-config-interceptor/http-config.interceptor.ts
+++ src/app/core/interceptors/http-config-interceptor/http-config.interceptor.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export default class HttpConfigInterceptor implements HttpInterceptor {
+  // eslint-disable-next-line class-methods-use-this
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request);
   }
