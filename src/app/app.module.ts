310b1d5094144083c7ea80c7e8a65289df905660
Ashutosh Bisoyi <108815380+ashutosh-bisoyi@users.noreply.github.com>
2023-11-24 12:22:56 +0530

    ci: add github CI setup (#4)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import HttpLoaderFactory from 'src/assets/i18n/loader';
+import { BrowserModule } from '@angular/platform-browser';
 import AppComponent from './app.component';
 import AppRoutingModule from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
