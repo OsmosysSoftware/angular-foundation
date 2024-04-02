75552f26237b367e153340d5336d1dc04be345ab
kshitij.k <kshitij.k@osmosys.co>
2024-04-02 20:44:29 +0530

    feat: error handling for base path

--- src/assets/i18n/loader.ts
+++ src/assets/i18n/loader.ts
@@ -1,7 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-const basePath = document.getElementsByTagName('base')[0].href;
+let basePath = '';
+const baseElements = document.getElementsByTagName('base');
+
+if (baseElements.length > 0 && baseElements[0].href) {
+  basePath = baseElements[0].href;
+} else {
+  basePath = '../';
+  // eslint-disable-next-line no-console
+  console.error('No <base> tag found or missing href attribute.');
+}
 
 function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, `${basePath}assets/i18n/`, '.json');
