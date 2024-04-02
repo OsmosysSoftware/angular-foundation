10ffe801658cb4c97e7399e0e735accf577b507a
kshitij-k-osmosys <121787214+kshitij-k-osmosys@users.noreply.github.com>
2024-04-02 21:21:53 +0530

    fix: i18n basePath for github pages (#20)

--- src/assets/i18n/loader.ts
+++ src/assets/i18n/loader.ts
@@ -1,8 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+let basePath = '';
+const baseElements = document.getElementsByTagName('base');
+
+if (baseElements.length > 0 && baseElements[0].href) {
+  basePath = baseElements[0].href;
+} else {
+  basePath = '../';
+  // eslint-disable-next-line no-console
+  console.error('No <base> tag found or missing href attribute.');
+}
+
 function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, `${basePath}assets/i18n/`, '.json');
 }
 
 export default HttpLoaderFactory;
