ef6562f052f2e697cc0cc77decee63055c62b164
kshitij.k <kshitij.k@osmosys.co>
2024-04-02 18:48:41 +0530

    fix: i18n basePath for github pages

--- src/assets/i18n/loader.ts
+++ src/assets/i18n/loader.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+const basePath = document.getElementsByTagName('base')[0].href;
+
 function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, `${basePath}assets/i18n/`, '.json');
 }
 
 export default HttpLoaderFactory;
