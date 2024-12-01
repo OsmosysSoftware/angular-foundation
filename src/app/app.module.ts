d98b3823923cf08f875f923cee6debe6f1a16142
Hitesh <hitesh.k@osmosys.co>
2024-12-02 00:10:46 +0530

    add primeng toast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,11 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import HttpLoaderFactory from 'src/assets/i18n/loader';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastModule } from 'primeng/toast';
 import AppComponent from './app.component';
 import AppRoutingModule from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +17,9 @@ import AppRoutingModule from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    ToastModule,
+
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
@@ -22,7 +28,7 @@ import AppRoutingModule from './app-routing.module';
       },
     }),
   ],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }, MessageService],
   bootstrap: [AppComponent],
 })
 export default class AppModule {}
